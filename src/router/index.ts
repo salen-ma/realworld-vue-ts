30f8384f4342e9d80f8f3701fbdb33d310a95128
mashaoliang <salen.ma@qq.com>
2020-08-06 08:59:04 +0800

    feat: init realworld-vue-ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,16 +7,19 @@ Vue.use(VueRouter)
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('@/views/layout/index.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import(/* webpackChunkName: "home" */ '@/views/home/index.vue')
+      },
+      {
+        path: '/login',
+        name: 'Login',
+        component: () => import(/* webpackChunkName: "login" */ '@/views/login/index.vue')
+      }
+    ]
   }
 ]
 
