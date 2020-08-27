2b8c97250e5d2127b09324fb8b343785d5d2cdb6
mashaoliang <salen.ma@qq.com>
2020-08-27 23:36:57 +0800

    feat: add ts comment

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,10 @@
 import dayjs from 'dayjs'
 
+/**
+ * @param {string} value - 日期
+ * @param {string} [fromat="YYYY-MM-DD"] - 日期格式
+ * @return {string} 格式化后的日期
+ */
 export const dateFormat = (value: string, fromat = 'YYYY-MM-DD') => {
   return dayjs(value).format(fromat)
 }
